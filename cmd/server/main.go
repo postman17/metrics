e69cb7756590c2e398af584c44604a07e284c7f4
Konstantin Morozov <konstantin.morozov@vk.team>
2026-04-24 07:32:27 +0300

    iter3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	handlers "github.com/postman17/metrics/internal/handler"
 	repo "github.com/postman17/metrics/internal/repository"
 )
 
 func main() {
 	memory := repo.NewMemStorage()
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/update/{type}/{name}/{value}`, handlers.UpdateMetricPage(memory))
 
-	err := http.ListenAndServe(`:8080`, mux)
+	r := chi.NewRouter()
+
+	r.Get("/", handlers.GetMainPage(memory))
+	r.Post("/update/{type}/{name}/{value}", handlers.UpdateMetricPage(memory))
+	r.Get("/value/{type}/{name}", handlers.GetMetricValuePage(memory))
+
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		panic(err)
 	}
---
+++ internal/handler/main_mage.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	mem "github.com/postman17/metrics/internal/repository"
+)
+
+const htmlPage = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Metrics List</title>
+</head>
+<body>
+    <h1>Current Metrics</h1>
+    <ul>
+        {{range $name, $value := .}}
+            <li><strong>{{$name}}</strong>: {{$value}}</li>
+        {{end}}
+    </ul>
+</body>
+</html>`
+
+func GetMainPage(memory *mem.MemStorage) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			rw.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		rw.Header().Set("Content-Type", "text/html")
+		rw.WriteHeader(http.StatusOK)
+		template := template.Must(template.New("metrics").Parse(htmlPage))
+		err := template.Execute(rw, memory.Data)
+		if err != nil {
+			fmt.Println("Ошибка шаблона:", err)
+		}
+	}
+}
