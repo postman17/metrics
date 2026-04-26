067f936fb8f54748be37e3519a2263240308ffc2
Konstantin Morozov <konstantin.morozov@vk.team>
2026-04-26 09:39:38 +0300

    fix http scheme

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"flag"
+	"strings"
 )
 
 var (
-	flagRunAddr        string
 	flagReportInterval int64
 	flagPollInterval   int64
+	flagRunAddr        httpAddr = "http://localhost:8080"
 )
 
+type httpAddr string
+
+func (h *httpAddr) Set(s string) error {
+	if !strings.HasPrefix(s, "http://") && !strings.HasPrefix(s, "https://") {
+		s = "http://" + s
+	}
+	*h = httpAddr(s)
+	return nil
+}
+
+func (h *httpAddr) String() string {
+	return string(*h)
+}
+
 func parseFlags() {
-	flag.StringVar(&flagRunAddr, "a", "http://localhost:8080", "address and port to run server")
+	flag.Var(&flagRunAddr, "a", "address and port to run server")
 	flag.Int64Var(&flagReportInterval, "r", 10, "reportInterval")
 	flag.Int64Var(&flagPollInterval, "p", 2, "pollInterval")
 	flag.Parse()
