721eea0aeb5591f2bffd96d1d0eab106480e5423
Konstantin Morozov <konstantin.morozov@vk.team>
2026-05-20 08:56:59 +0300

    fix gzip test

--- internal/gzip/gzip_test.go
+++ internal/gzip/gzip_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestGzipCompression(t *testing.T) {
-	memory := repo.NewMemStorage()
+	memory := repo.NewMemStorage(true, "")
 	handler := GZIPMiddleware(handlers.UpdateMetric(memory))
 
 	srv := httptest.NewServer(handler)
